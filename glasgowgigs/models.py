5f2d82fed103389f78d9a1117d21d806c23363c3
daniela997 <2262058i@student.gla.ac.uk>
2018-03-09 11:29:05 +0000

    events model fixed

--- glasgowgigs/models.py
+++ glasgowgigs/models.py
@@ -19,7 +19,7 @@ class Venue(models.Model):
         return self.name
 
 class Event(models.Model):
-    name = 
+    name = models.CharField(max_length=128)
     venue = models.ForeignKey(Venue)
     artist = models.ForeignKey(Artist)
     date = models.DateField()
@@ -31,6 +31,6 @@ class Event(models.Model):
         return self.name
     def save (self, *args, **kwargs):
         if self.venue and self.artist:
-            self.name = self.artist+" @ "+self.venue
+            self.name = self.artist.name+" @ "+self.venue.name
         super(Event, self).save(*args, **kwargs)
 
