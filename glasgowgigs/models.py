7e7a8f70b9fc61f75e7cbc7d585dc6d1ed7a9e99
daniela997 <2262058i@student.gla.ac.uk>
2018-03-09 11:00:37 +0000

    models test

---
+++ glasgowgigs/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    address = models.CharField(max_length=128, unique=True) 
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+
+class Event(models.Model):
+    name = 
+    venue = models.ForeignKey(Venue)
+    artist = models.ForeignKey(Artist)
+    date = models.DateField()
+    bookings = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+    def save (self, *args, **kwargs):
+        if self.venue and self.artist:
+            self.name = self.artist+" @ "+self.venue
+        super(Event, self).save(*args, **kwargs)
+
