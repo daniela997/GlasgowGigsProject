d43685fb297e4594706e01add260304a8d6c8a11
Martina Lof <martinalofqvist@outlook.com>
2018-03-09 10:29:02 +0000

    M commit

--- glasgowgigs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from glasgowgigs.models import Artist, Venue, Event
-
-admin.site.register(Artist)
-admin.site.register(Venue)
-admin.site.register(Event)
--- glasgowgigs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GlasgowgigsConfig(AppConfig):
-    name = 'glasgowgigs'
