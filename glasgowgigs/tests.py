873e330333c9da21794bdc2fb153278c4681fd52
Martina Lof <martinalofqvist@outlook.com>
2018-03-21 17:50:56 +0000

    test.py added

--- glasgowgigs/tests.py
+++ glasgowgigs/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from glasgowgigs.models import Artist
+from django.test import Client
 
-# AboutView.as_views
+class AnimalTestCase(TestCase):
+
+    def setUp(self):
+        #Artist.objects.create()
+        self.c = Client()
+
+    def test_animals_can_speak(self):
+        self.assertEqual(True, True)
+
+        c = Client()
+        response = c.get('/artists', follow=True)
+        response.status_code
+        print("content", response.content)
+        print("status_code", response.status_code)
