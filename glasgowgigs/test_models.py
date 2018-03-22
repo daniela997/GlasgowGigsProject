aa8536790fef75ce686c91a5f280dbbd8497fc8c
Martina Lofqvist <34751844+MartinaLof@users.noreply.github.com>
2018-03-22 19:14:46 +0000

    Update test_models.py

--- glasgowgigs/test_models.py
+++ glasgowgigs/test_models.py
@@ -3,7 +3,7 @@
 # Clear population script first
 
 from django.test import TestCase
-from glasgowgigs.models import Artist
+from glasgowgigs.models import Artist, Venue, Event
 from django.test import Client
 
 # Tests in case of no population script
@@ -23,3 +23,37 @@ class ArtistTestCase(TestCase):
         print("status_code", response.status_code)
 
 # Test if there is no video
+
+# Tests in case of no population script
+class VenuesTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_venues_content_empty(self):
+        self.assertEqual(True, True)
+
+        c = Client()
+        response = c.get('/artists', follow=True)
+        response.status_code
+        print("content", response.content)
+        print("status_code", response.status_code)
+        
+        
+# Tests in case of no population script
+class EventsTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_events_content_empty(self):
+        self.assertEqual(True, True)
+
+        c = Client()
+        response = c.get('/artists', follow=True)
+        response.status_code
+        print("content", response.content)
+        print("status_code", response.status_code)
+        
+        
+        
